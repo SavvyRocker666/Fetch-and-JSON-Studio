5251c81c85196fa50bb277d7bf1154018d74b5fe
Savvy <sabrinarocker16@gmail.com>
2022-02-14 20:25:43 -0600

    updates

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// TODO: add code here
+window.addEventListener("load", function (){
+ this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response){
+    response.json().then(function(json) {
+       console.log(json);
+       const div= document.getElementById("container");
+       let counter = 0;
+       for (let i=0; i < json.length; i++){
+          div.innerHTML+= `
+          <div class ="astronaut">
+          <div class ="bio">
+          <h3> `
+       }
+    }
+ }
+}
